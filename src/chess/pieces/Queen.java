34a92bcdd593a34bcf787bbdb0c0677699b9a4a2
Vinicius Guimarães <vinijipa23@gmail.com>
2019-05-18 19:52:19 -0400

    Queen (and Pawn bugfix)

--- src/chess/pieces/Queen.java
+++ src/chess/pieces/Queen.java
@@ -1,29 +1,83 @@
 package chess.pieces;
 
+
 import boardgame.Board;
 import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
-public class Bishop extends ChessPiece {
+public class Queen extends ChessPiece {
 
-	public Bishop(Board board, Color color) {
+	public Queen(Board board, Color color) {
 		super(board, color);
 
 	}
 
 	@Override
 	public String toString() {
-		return "B";
+		return "Q";
 	}
 
 	@Override
 	public boolean[][] possibleMoves() {
-		// TODO Auto-generated method stub
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 
 		Position p = new Position(0, 0);
+		
+		//Posições iguais a da torre
+		// Cima
+		p.setValues(position.getRow() - 1, position.getColumn());
+		// Enquanto a posicção existir e não tiver uma peça lá, faça tal coisa
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() - 1);
 
+		}
+		// Quando terminar as posições fazias, verifica se a peça é amiga ou adversária
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// Esquerda
+		p.setValues(position.getRow(), position.getColumn() - 1);
+
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setColumn(p.getColumn() - 1);
+
+		}
+
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// Direita
+		p.setValues(position.getRow(), position.getColumn() + 1);
+
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setColumn(p.getColumn() + 1);
+
+		}
+
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// Para Baixo
+		p.setValues(position.getRow() + 1, position.getColumn());
+
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() + 1);
+
+		}
+
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Posições iguais a do bispo
 		// Noroeste
 		p.setValues(position.getRow() - 1, position.getColumn() - 1);
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
@@ -38,8 +92,9 @@ public class Bishop extends ChessPiece {
 
 		// Nordeste
 		p.setValues(position.getRow() - 1, position.getColumn() + 1);
-		
-		//Enquanto a posição existir e não tiver uma peça nela, continue colocando o valor da matriz como true
+
+		// Enquanto a posição existir e não tiver uma peça nela, continue colocando o
+		// valor da matriz como true
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 			p.setValues(p.getRow() - 1, p.getColumn() + 1);
@@ -52,8 +107,9 @@ public class Bishop extends ChessPiece {
 
 		// Sudoeste
 		p.setValues(position.getRow() + 1, position.getColumn() - 1);
-		
-		//Se a posição existe e não tem uma peça lá, coloque esta posição na matriz (mat) como verdadeira
+
+		// Se a posição existe e não tem uma peça lá, coloque esta posição na matriz
+		// (mat) como verdadeira
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 			p.setValues(p.getRow() + 1, p.getColumn() - 1);
@@ -75,6 +131,7 @@ public class Bishop extends ChessPiece {
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
+
 		return mat;
 	}
 }
